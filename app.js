e98d935cf615933d380e1dbc83c0177ccb210c54
Twiggeh <twiggeh99@gmail.com>
2020-08-05 23:11:06 +0200

    bot logs message

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-import Discord from 'discord.js';
 const client = new Discord.Client();
+
+client.on('ready', () => console.log('Bot online'));
+
+client.on('message', msg => {
+  console.log(msg);
+});
+
+client.login(botKey);
+
+import { botKey } from './config/keys.js';
+import Discord from 'discord.js';
