237dc1bf6627bd6b76b27c8cfaa2962333f51f3c
Twiggeh <twiggeh99@gmail.com>
2020-08-10 09:08:28 +0200

    added confirm and timeout clearing

--- app.js
+++ app.js
@@ -2,28 +2,46 @@ export const client = new Discord.Client();
 
 const coaches = ['145856913014259712'];
 const IS_REPLAY_POOL = createPool('IS_REPLAY_POOL');
+const QUEUE_POOL = createPool('QUEUE_POOL');
 
 client.on('ready', () => console.log('Bot online'));
 
 client.on('messageReactionAdd', async (msgReact, user) => {
   if (user.bot) return;
   const msgInPool = isPartOfPool(msgReact.message.id);
-  // TODO : Lock the interaction down. Here I don't have to acknowledge that a message might have a history since IS_REPLAY_POOL has immediate consequences to a reaction.
   // User has reacted
   switch (msgInPool) {
     case 'IS_REPLAY_POOL': {
+      // TODO : Lock the interaction down. Here I don't have to acknowledge that a message might have a history since IS_REPLAY_POOL has immediate consequences to a reaction.
       switch (msgReact._emoji.name) {
         case '✅': {
+          // TODO : add coachProvider
+          clearTimeout(IS_REPLAY_POOL[msgReact.message.id].timeOutId);
+          await IS_REPLAY_POOL[msgReact.message.id].origMsg.delete();
+          await msgReact.message.channel.send(isSC2Replay(1));
+          console.log(IS_REPLAY_POOL[msgReact.message.id]);
+          await sleep(10 * 1000);
+          await delAllMsgs({ DMChannels: [msgReact.message.channel] });
           return;
         }
         case '🛑': {
+          clearTimeout(IS_REPLAY_POOL[msgReact.message.id].timeOutId);
           await msgReact.message.channel.send(isNotSC2Replay);
           await sleep(10 * 1000);
           await delAllMsgs({ DMChannels: [msgReact.message.channel] });
           return;
         }
+        default:
+          return console.log(
+            'User tried to provide wrong emote : ' + msgReact._emoji.name
+          );
       }
     }
+    case 'QUEUE_POOL': {
+      return;
+    }
+    default:
+      return console.log(`POOL ${msgInPool} not implemented`);
   }
 });
 
@@ -38,7 +56,7 @@ client.on('message', async msg => {
     const content = msg.content;
     try {
       const answer = await msg.author.send(confirmIsReplayMsg);
-      buildTicket(IS_REPLAY_POOL, { id: answer.id, content, url });
+      buildTicket(IS_REPLAY_POOL, { id: answer.id, content, url, origMsg: msg });
       await answer.react('✅');
       await answer.react('🛑');
     } catch (e) {
@@ -53,6 +71,6 @@ import { botKey } from './config/keys.js';
 import Discord, { DMChannel } from 'discord.js';
 import { sleep, shouldHandleMsg, buildTicket, POOLS, createPool } from './utils.js';
 import { writeFileSync, readFileSync } from 'fs';
-import { confirmIsReplayMsg, isNotSC2Replay } from './messages.js';
+import { confirmIsReplayMsg, isNotSC2Replay, isSC2Replay } from './messages.js';
 import { isPartOfPool } from './utils.js';
 import { delAllMsgs } from './utils.js';
