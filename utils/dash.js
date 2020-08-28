293af31bf67703407aa512d3eb0334d602267092
Twiggeh <twiggeh99@gmail.com>
2020-08-28 21:32:36 +0200

    created SelectStudent and finishCoachingStudent

--- utils/dash.js
+++ utils/dash.js
@@ -1,3 +1,5 @@
+// TODO : refresh all dashboards after some time has passed (5min ?)
+
 /** Does not auto create a DM, just sends a dashboard and returns the dashboard message
  *  @param {DiscordUser} discordCoach*/
 const createDashboard = async discordCoach => {
@@ -14,6 +16,8 @@ const createDashboard = async discordCoach => {
  * @returns {Promise<Message>}
  */
 export const getDashboard = async discordCoach => {
+  // TODO : Disallow the creation of a dashboard if the user does not have the right permissions.
+  // !!! IMPORTANT Disallow the creation of a dashboard if the user does not have the right permissions.
   const cache = await Promise.allSettled([
     getDBCoach(discordCoach.id),
     discordCoach.dmChannel.messages.fetch(),
@@ -29,7 +33,7 @@ export const getDashboard = async discordCoach => {
     await coach.save();
   }
 
-  buildTicket(DASHBOARD_POOL, { id: dashboard.id });
+  buildTicket(DASHBOARD_POOL, { id: dashboard.id, coachID: discordCoach.id });
 
   return dashboard;
 };
@@ -54,12 +58,6 @@ const updateDashboards = async discordCoaches => {
   await Promise.allSettled(cache2);
   // TODO : add error handlers on all "allSettled" Promise handlers
 };
-
-// const updateAvailCoaches = async () => {
-//   const availDashes = await getAvailDashboards();
-//   await updateDashboards(availDashes.map(el => el.channel.recipient));
-//   console.log('done');
-// };
 
 export const updateAllDashboards = async () => {
   // TODO PUT INTO A PROVIDER
@@ -120,7 +118,7 @@ export const putAllReactsOnDash = async dash => {
       actions.push(() => dash.react(emojiObj[emojiName].id));
     }
   };
-  check(emojiIdentifiers);
+  check(numberIdent);
   check(reqDashEmojis);
   if (!missing) return;
 
@@ -194,20 +192,92 @@ export const goToPrevPage = (dashTicket, emoji, msgReact) => {
   msg.edit(dashboardMessage(msg.channel.recipient, curPage - 1));
 };
 
-export const selectStudent = () => {
+/**
+ * @type {import('./emojiInteraction.js').EmojisAndMethods["onAdd"]}
+ * @param {import('./ticket.js').D_Ticket} dashTicket
+ */
+export const selectStudent = async (dashTicket, emoji, msgReact) => {
+  // TODO:
+  // Wait for at least 2 mins => if aborted, ask if was an actual coaching attempt.
+  // |=> If yes fast forward to asking the coach and student about the experience. Collect data
+  //
+
+  const QUEUE_KEYS = Object.keys(QUEUE_POOL);
+  const numIdent = emojiIdent[emoji].id;
+
+  let index = -1;
+  for (let i = 0; i < QUEUE_KEYS.length; i++) {
+    index += (QUEUE_POOL[QUEUE_KEYS[i]].emojiIdentifier === numIdent) * (i + 1);
+  }
+  /** @type {import('./ticket.js').Q_Ticket} */
+  const quTicket = QUEUE_POOL[QUEUE_KEYS[index]];
+
+  if (!quTicket)
+    return console.log("Coach tried to select a emoji that doesn't have a student on it");
+  // don't allow other coaches to select this student
+  if (quTicket.coach) return console.log('Already being coached');
+
+  // Mark the student as being coached
+  quTicket.coach = msgReact.message.channel.recipient;
+  quTicket.startedCoaching = Date.now();
+
+  // TODO : Send the Url, the userId and the original message to the coach.
+  // TODO : After coaching remove these messages.
+
+  // Update QUEUE_POOL's ids
+  updateQueuePool();
+
+  /** @type {import('../Models/Queue_Pool.js').QPE_Opts} */
+  const queuePoolEntry = await Queue_PoolEntry.findOne({ id: quTicket.id });
+  queuePoolEntry.coachID = msgReact.message.channel.recipient.id;
+  queuePoolEntry.startedCoaching = Date.now();
+  await queuePoolEntry.save();
+
+  // Refresh all dashboards
+  await updateAllDashboards();
+
   console.log('Selected student');
 };
 
-export const finishedCoachingStudent = () => {
+/**
+ * @type {import('./emojiInteraction.js').EmojisAndMethods["onDel"]}
+ * @param {import('./ticket.js').D_Ticket} dTicket
+ */
+export const finishedCoachingStudent = async (dTicket, msgReact) => {
+  // Ask user whether coaching has succeeded.
+  /** @type {import('./ticket.js').Q_Ticket} */
+  const qTicket = QUEUE_POOL[dTicket.studentQTicketID]; // TODO : dTicket.studentQTicketID is still undefined
+  const answer = await qTicket.student.send(successfulCoaching);
+  /** @type {import('./ticket.js').CL_Ticket} */
+  const options = {
+    id: answer.id,
+    coachID: dTicket.coachID,
+    dTicket,
+    qTicket,
+    studentQTicketID: dTicket.studentQTicketID,
+  };
+  await buildTicket(COACHLOG_POOL, options);
+
+  // If yes, save to the database
+
+  // if no, remove elements from queue_pool and reload queue_pool and the dashboards of the coaches
+
+  // free the coaches dashboard
+  // dTicket.studentQTicketID = undefined;
+  // dTicket.startedCoaching = undefined;
+  // dTicket.lockedEmojiInteractionGroups = [];
+  // TODO : Free up the student's QUEUE_TICKET if he was satisfied, if not reset it.
   console.log('Finished Coaching');
 };
 
-import { dashboardMessage } from '../messages.js';
+import { dashboardMessage, successfulCoaching } from '../messages.js';
 import { getDBCoach } from './coach.js';
 import { client } from '../app.js';
-import { emojiIdentifiers, reqDashEmojis } from '../Emojis.js';
+import { numberIdent, emojiIdent, reqDashEmojis } from '../Emojis.js';
 import Coach from '../Models/Coach.js';
 import { getStrUTCDay, filterNum } from './utils.js';
 import { Message, User as DiscordUser } from 'discord.js';
-import { DASHBOARD_POOL } from '../init.js';
+import { DASHBOARD_POOL, QUEUE_POOL, COACHLOG_POOL } from '../init.js';
 import { buildTicket } from './ticket.js';
+import Queue_PoolEntry from '../Models/Queue_Pool.js';
+import { updateQueuePool } from './pool.js';
