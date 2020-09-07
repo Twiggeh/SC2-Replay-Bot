6aac2eabf89c37b2703316a8b3f7108ce0c68c43
Twiggeh <twiggeh99@gmail.com>
2020-09-07 23:25:09 +0200

    added more aliases

--- config/global.js
+++ config/global.js
@@ -2,14 +2,14 @@ export const zShortcuts = ['[zerg]', '[z]'];
 export const pShortcuts = ['[protoss]', '[p]', '[toss]'];
 export const tShortcuts = ['[terran]', '[t]'];
 
-export const zVsShortcuts = ['[vszerg]', '[vsz]'];
-export const pVsShortcuts = ['[vsprotoss]', '[vsp]', '[vstoss]'];
-export const tVsShortcuts = ['[vsterran]', '[vst]'];
+export const zVsShortcuts = ['[vz]', '[vszerg]', '[vsz]'];
+export const pVsShortcuts = ['[vp]', '[vsprotoss]', '[vsp]', '[vstoss]'];
+export const tVsShortcuts = ['[vt]', '[vsterran]', '[vst]'];
 
-export const replayCuts = ['[isreplay]'];
+export const replayCuts = ['[ir]', '[isreplay]'];
 
-export const bRankCuts = ['[bronze]'];
-export const sRankCuts = ['[silver]'];
-export const gRankCuts = ['[gold]'];
-export const pRankCuts = ['[plat]', '[platinum]'];
-export const dRankCuts = ['[diamond]', '[dia]'];
+export const bRankCuts = ['[b]', '[bronze]'];
+export const sRankCuts = ['[s]', '[silver]'];
+export const gRankCuts = ['[g]', '[gold]'];
+export const pRankCuts = ['[pl]', '[plat]', '[platinum]'];
+export const dRankCuts = ['[d]', '[dia]', '[diamond]'];
