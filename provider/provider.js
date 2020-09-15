18c2f9268553ceb51ba73b582fe7e6745d8e5978
Twiggeh <twiggeh99@gmail.com>
2020-09-15 15:28:55 +0200

    better provider, cant spawn more than 1 instance

--- provider/provider.js
+++ provider/provider.js
@@ -1,11 +1,3 @@
-export const ingestDataFromFile = async fileName =>
-  new Promise((res, rej) =>
-    readFile(fileName, (err, data) => {
-      if (err) rej(err);
-      res(data.toString().split('\n'));
-    })
-  );
-
 const zergQuery = ['ZergCoach', '556487007535366174'];
 const tossQuery = ['TossCoach', '556487013818302464'];
 const terQuery = ['TerCoach', '556487011066707968'];
@@ -20,47 +12,72 @@ export const coachRoles = [
 ];
 const coachQuery = ['Coach', '546799895840030736', 'AspiringCoach', '552128337766514690'];
 
-let lastRetrieved;
-if (lastRetrieved === undefined) lastRetrieved = Date.now();
-let memoizedIds;
+let lastRetrieved = 0;
+let memoizedIds = [];
+let fetching = false;
 
 export const getCoaches = async (
   { zer = false, tos = false, ter = false, allCoach = false } = { allCoach: true }
 ) => {
-  if (memoizedIds !== undefined && Date.now() - lastRetrieved < 15 * 60 * 1000) {
-    console.log(
-      `Have to wait for ${
-      15 * 60 * 1000 - Date.now() + lastRetrieved
-      } before I will fetch new coaches`
-    );
-    return memoizedIds;
+  const lock = {};
+  if (15 * 60 * 1000 < Date.now() - lastRetrieved && !fetching) {
+    // need to fetch
+    const finalQuery = [];
+    (() => {
+      if (allCoach) return finalQuery.push(...coachQuery);
+      if (zer) finalQuery.push(...terQuery);
+      if (tos) finalQuery.push(...tossQuery);
+      if (ter) finalQuery.push(...zergQuery);
+    })();
+
+    // prettier-ignore
+    const args = ['app', 'tUser', 'tAmount', 'tTitle', 'tPrettyPrint', 'tPrintUserId', 'sRole', JSON.stringify(finalQuery)];
+    const offlineMembers = spawn('node', args, {
+      cwd: process.cwd() + '/OfflineMembersBot',
+    });
+
+    // promisify childProcess
+    const fetchIds = () =>
+      new Promise((res, rej) => {
+        lock.res = res;
+        lock.rej = rej;
+        const result = [];
+        offlineMembers.stdout.on('data', data => {
+          result.push(...data.toString().split('\n'));
+        });
+        offlineMembers.stderr.on('data', data => {
+          console.error(data.toString());
+        });
+        offlineMembers.on('close', code => {
+          if (code !== 0) rej(code);
+          memoizedIds = result.filter(i => !!i);
+          res(memoizedIds);
+        });
+      });
+    lock.p = fetchIds();
+    const ids = await lock.p;
+    if (ids === undefined) {
+      console.error(
+        'fetchIds in getCoaches did not retrieve any coaches, sending outdated data'
+      );
+      return memoizedIds;
+    }
+    lastRetrieved = Date.now();
+    memoizedIds = ids;
   }
-  const finalQuery = [];
-  (() => {
-    if (allCoach) return finalQuery.push(...coachQuery);
-    if (zer) finalQuery.push(...terQuery);
-    if (tos) finalQuery.push(...tossQuery);
-    if (ter) finalQuery.push(...zergQuery);
-  })();
+  if (fetching) {
+    console.log('Waiting for previous fetch.');
+    await lock.p;
+  }
 
-  // prettier-ignore
-  const args = ['app', 'tUser', 'tAmount', 'tTitle', 'tPrettyPrint', 'tPrintUserId', 'sRole', JSON.stringify(finalQuery)];
-  const offlineMembers = spawn('node', args, {
-    cwd: process.cwd() + '/OfflineMembersBot',
-  });
-
-  return new Promise((res, rej) => {
-    const result = [];
-    offlineMembers.stdout.on('data', data => {
-      result.push(...data.toString().split('\n'));
-    });
-    offlineMembers.on('close', code => {
-      if (code !== 0) rej(code);
-      memoizedIds = result.filter(i => !!i);
-      res(memoizedIds);
-    });
-  });
+  // don't need to fetch, data is fresh enough
+  console.log(
+    `Have to wait for ${
+      15 * 60 * 1000 - Date.now() + lastRetrieved
+    } before I will fetch new coaches`
+  );
+  console.log('memoizedIds', memoizedIds);
+  return memoizedIds;
 };
 
-import { readFile } from 'fs';
 import { spawn } from 'child_process';
